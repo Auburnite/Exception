64561ac61bdd6f03197307ad6ad2e99d534af5b4
jmwamser <jwamser@redpandacoding.com>
2024-01-15 02:07:17 +0000

    add all libs to workflow

---
+++ LogicException.php
@@ -0,0 +1,16 @@
+<?php
+
+/*******************************************************************************
+ * Auburnite
+ *
+ * @link                https://github.com/Auburnite/Auburnite
+ * @copywrite           Copywrite (c) 2023-present | Jordan Wamser - RedPanda Coding
+ * @license             https://github.com/Auburnite/Auburnite/blob/main/LICENSE
+ ******************************************************************************/
+namespace Auburnite\Auburnite\Component\Exception;
+
+use Auburnite\Auburnite\Contracts\Exception\ExceptionInterface;
+
+class LogicException extends \LogicException implements ExceptionInterface
+{
+}
